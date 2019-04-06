d726ebefc17f24a47e640c8562eed4650f951fa7
Xavier Nhagumbe <xavierfrancisco353@gmail.com>
2019-04-06 11:03:40 +0200

    Initialize...

---
+++ src/main/java/mz/com/xavier/mozebusness/MozebusnessApplication.java
@@ -0,0 +1,13 @@
+package mz.com.xavier.mozebusness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MozebusnessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MozebusnessApplication.class, args);
+	}
+
+}
